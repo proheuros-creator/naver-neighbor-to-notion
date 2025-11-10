a5b8d72d85a9fa9b9f8dde22a6b1cdc9fd10dd9f
proheuros-creator <proheuros@gmail.com>
2025-11-10 17:29:02 +0900

    Update groups.js

--- groups.js
+++ groups.js
@@ -7,16 +7,16 @@
  */
 
 export const GROUPS = [
-  { id: 1, name: "투자(daily)" },
-  { id: 7, name: "투자(insight)" },
-  { id: 14, name: "투자(정리)" },  
-  { id: 13, name: "투자(Diligent)" },
-  { id: 2, name: "투자" }
-  { id: 9, name: "에코" },
-  { id: 3, name: "미국투자" },  
-  { id: 5, name: "중국투자" },  
   { id: 4, name: "증권사" },  
   { id: 8, name: "투자(Youtube)" }, 
+  { id: 5, name: "중국투자" },  
+  { id: 3, name: "미국투자" }, 
+  { id: 9, name: "에코" },
+  { id: 2, name: "투자" }
+  { id: 13, name: "투자(Diligent)" },
+  { id: 14, name: "투자(정리)" },  
+  { id: 7, name: "투자(insight)" },
+  { id: 1, name: "투자(daily)" },
   // ✅ 새 그룹을 추가하려면 아래처럼 한 줄만 추가하세요.
   // { id: 15, name: "새그룹" },
 ];
