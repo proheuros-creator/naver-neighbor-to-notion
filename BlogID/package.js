362db067c627e15bbc752c82a51a407302c423f7
proheuros-creator <proheuros@gmail.com>
2025-11-11 15:45:35 +0900

    Create package.js

---
+++ BlogID/package.js
@@ -0,0 +1,12 @@
+{
+  "name": "blogid-tools",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": {
+    "sync:blogid": "node sync-blogid-from-csv.js"
+  },
+  "dependencies": {
+    "@notionhq/client": "^2.2.10",
+    "csv-parse": "^5.5.6"
+  }
+}
