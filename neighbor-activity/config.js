6583423ed2a1a5add0200b87254307bee14efbb6
proheuros-creator <proheuros@gmail.com>
2025-11-11 12:00:08 +0900

    Update config.js

--- neighbor-activity/config.js
+++ neighbor-activity/config.js
@@ -1,13 +1,7 @@
 // neighbor-activity/config.js
-
 module.exports = {
-  // 네이버 블로그 이웃 관리 화면 기본 URL
-  adminBuddyUrl:
-    "https://admin.blog.naver.com/AdminMain.naver?blogId=proheuros&Redirect=Buddyinfo",
-
-  // 페이지 최대 탐색 수 (실제보다 넉넉히, 중간에 자동 종료함)
-  maxPages: 30,
-
-  // 블로그 하나 긁고 다음으로 넘어가기 전 대기(ms)
+  baseId: "proheuros",     // 네 blogId
+  startPage: 1,
+  endPage: 10,              // 네 화면에 보이는 마지막 페이지 번호
   delayMs: 1500
 };
